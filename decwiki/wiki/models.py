a7ea7cdc082f7431819e9c4ba7624dc38c51b906
Satyam <139366115+SatyamPote@users.noreply.github.com>
2024-05-18 12:40:56 +0530

    trial1

---
+++ decwiki/wiki/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class EditHistory(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    edited_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    edited_at = models.DateTimeField(auto_now_add=True)
+    old_content = models.TextField()
+
+    def __str__(self):
+        return f"{self.article.title} edited by {self.edited_by.username} at {self.edited_at}"
